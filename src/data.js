3ec5cfa7164fd351fac1f12cfea2113fd2bb5be2
Reena <reena.sanjay.sharma@gmail.com>
2018-11-18 14:13:41 +0000

    added experience data in data file

--- src/data.js
+++ src/data.js
@@ -1,3 +1,40 @@
-const data = { about: "I am a Software Developer who loves to experiment and challenge herself."}
+const data = {
+  about: "I am an enthusiastic and passionate Junior Full Stack Developer who was working at Pottermore where I gained hands-on experience in web development. Before this, I was undergoing training at Makers, one of Europe’s leading and most selective web development bootcamp. At Makers, I learnt Software Craftsmanship, Object Oriented Programming, TDD and Agile methodologies. Prior to joining Makers, I gained an analytical mindset and a high attention to detail from my Accounting and Finance background.",
+
+  experience: [
+    {
+      "CompanyName": "Pottermore(part of WWDFE)",
+      "title": "Junior Developer",
+      "duration": "July 2018 - Oct 2018",
+      "description": ["Web development in Javascript and React to create responsive web applications",
+        "Exposure to Groovy, Terraform, AWS, Contentful etc",
+        "Regularly checking web logs on Papertrail",
+        "Part of a scrum team with 2 week sprints and daily standups for status updates"
+      ]
+    },
+
+    {
+      "CompanyName": "Career Gap",
+      "title": "Relocated to United Kingdom",
+      "duration": "Jan 2015 to Oct 2017",
+      "description": ["Traveled",
+        "Explored my interest in programming"
+      ]
+    },
+
+    {
+      "CompanyName": "PRGX India Pvt Ltd",
+      "title": "Junior Auditor",
+      "duration": "Dec 2013 to Dec 2014",
+      "description": ["Matching clients accounting history to discover and recover over payments,         missed discounts and pricing errors",
+        "Analysing payment history to crosscheck if the allowances or payback were received from vendor, if not received then raising debit memo on behalf of the client",
+        "Preparing weekly reports to stakeholders",
+        "Identifying and conducting domain training/presentation for team members and handling their queries"
+      ]
+    }
+
+  ]
+
+}
 
 export default data;
